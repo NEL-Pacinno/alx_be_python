854a6ad1dab4a1b0d2e124488ca0df0e3330cf7d
Nelson Ejimadu <ejimadunelson@gmail.com>
2025-07-16 16:57:00 +0100

    Add simple_interest.py with arithmetic operations

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000  # Principal amount
+rate = 0.05 # Interest rate in percent
+time = 3  # Time in years
+interest = principal * rate * time
+print(f"The interest earned is: {interest}")
