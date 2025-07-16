ce54d7981db3ff5d60db40670b6fee08e74f229c
Nelson Ejimadu <ejimadunelson@gmail.com>
2025-07-16 16:12:31 +0100

    Add basic_operations.py with arithmetic operations

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("Sum of numbers:", number1, "and", number2, "is", sum)
+print("Difference of numbers:", number1, "and", number2, "is", difference)
+print("Product of numbers:", number1, "and", number2, "is", product)
+
+
