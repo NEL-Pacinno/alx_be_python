b054c1831695cf0b38e6b0cec6b05d6ba0ba943e
Nelson Ejimadu <ejimadunelson@gmail.com>
2025-07-16 17:34:40 +0100

    Add future_age_calculator.py with arithmetic operations

---
+++ future_age_calculator.py
@@ -0,0 +1,4 @@
+current_age = 30  # Current age in 2023
+age_in_2050 = 27  # Age in 2050
+future_age = current_age + age_in_2050
+print(f"In {age_in_2050} years, you will be {future_age} years old.")
